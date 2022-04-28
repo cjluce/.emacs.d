f9f59cd0bf77bdb374eb94f3784506e57d3ed584
cjluce <16cluce@gmail.com>
2022-04-28 19:43:00 -0400

    Removed some garbage, and added some lisp-specific editing niceties.

--- init.el
+++ init.el
@@ -17,11 +17,14 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
+(setq backup-directory-alist `((".*" . "~/.emacs.d/backups/")))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq inhibit-startup-screen t)
+
+;; Misc. Hooks
+(add-hook 'prog-mode-hook 'electric-pair-local-mode)
 
 ;; Install use-package
 (unless (package-installed-p 'use-package)
@@ -168,7 +171,7 @@
 
 
 (use-package lsp-mode
-  :init
+  ;; :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   ;; (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
@@ -177,95 +180,23 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
-;; (setq scheme-program-name "geiser")
+(use-package paredit
+  :hook ((emacs-lisp-mode . paredit-mode)
+	 (lisp-mode . paredit-mode)
+	 (scheme-mode . paredit-mode)))
 
+(use-package rainbow-delimiters
+  :hook ((prog-mode . rainbow-delimiters-mode)))
 
-(use-package geiser-guile
-  :config
-  (setq geiser-default-implementation 'guile))
-
-;; (setq scheme-program-name "geiser")
-(setq geiser-guile-binary "/bin/guile3")
-;; (setq geiser-active-implementations 'guile)
-;; (setq geiser-scheme-implementation 'guile)
-;; (setq geiser-guile-load-path "/bin/guile3")
-
-(require 'geiser-mode)
-(define-key geiser-mode-map (kbd "C-,") 'geiser-completion--complete-module)
-(define-key geiser-repl-mode-map (kbd "C-,") 'geiser-completion--complete-module)
-(define-key geiser-mode-map (kbd "C-.") 'other-window)
-(define-key geiser-repl-mode-map (kbd "C-.") 'other-window)
-
-
-;; (use-package geiser-mit
+;; (use-package telega
+;;   :ensure nil
 ;;   :config
-;;   (setq geiser-default-implementation 'mit))
-
-;; (setq geiser-active-implementations '(mit))
-;; (setq scheme-program-name "mit-scheme")
-
-;; (setq geiser-active-implementations '(guile))
-;; (setq geiser-scheme-implementation '(guile))
-;; (setq geiser-guile-load-path "/usr/bin/guile")
+;;   (setq telega-server-libs-prefix "/usr/include"))
 
 
+;; Add support for mitscheme
 
-;; (org-babel-scheme-execute-with-geiser "(+ 34 35)" t 'guile defun)
-
-
-;; (nil org-babel-scheme-execute-with-geiser (code output impl repl)
-;;   "Execute code in specified REPL.
-;; If the REPL doesn't exist, create it using the given scheme
-;; implementation.
-
-;; Returns the output of executing the code if the OUTPUT parameter
-;; is true; otherwise returns the last value."
-;;   (let ((result nil))
-;;     (with-temp-buffer
-;;       (insert (format ";; -*- geiser-scheme-implementation: %s -*-" impl))
-;;       (newline)
-;;       (insert code)
-;;       (geiser-mode)
-;;       (let ((geiser-repl-window-allow-split nil)
-;; 	    (geiser-repl-use-other-window nil))
-;; 	(let ((repl-buffer (save-current-buffer
-;; 			     (org-babel-scheme-get-repl impl repl))))
-;; 	  (when (not (eq impl (org-babel-scheme-get-buffer-impl
-;; 			       (current-buffer))))
-;; 	    (message "Implementation mismatch: %s (%s) %s (%s)" impl (symbolp impl)
-;; 		     (org-babel-scheme-get-buffer-impl (current-buffer))
-;; 		     (symbolp (org-babel-scheme-get-buffer-impl
-;; 			       (current-buffer)))))
-;; 	  (setq geiser-repl--repl repl-buffer)
-;; 	  (setq geiser-impl--implementation nil)
-;; 	  (let ((geiser-debug-jump-to-debug-p t)
-;; 		(geiser-debug-show-debug-p t))
-;; 	    (let ((ret (geiser-eval-region (point-min) (point-max))))
-;; 	      (setq result (if output
-;; 			       (or (geiser-eval--retort-output ret)
-;; 				   "Geiser Interpreter produced no output")
-;; 			     (geiser-eval--retort-result-str ret "")))))
-;; 	  (when (not repl)
-;; 	    (save-current-buffer (set-buffer repl-buffer)
-;; 				 (geiser-repl-exit))
-;; 	    (set-process-query-on-exit-flag (get-buffer-process repl-buffer) nil)
-;; 	    (kill-buffer repl-buffer)))))
-;;     result))
-
-
-;; (org-babel-scheme-execute-with-geiser "(define wowzabap \"wowzabap\")\nwowzabap" t 'guile nil)
-
-;; (org-babel-scheme-execute-with-geiser "\"HELLO WORLD\"" t 'mit nil)
-
-;; (org-babel-scheme-get-repl 'mit nil)
-
-;; (geiser:eval '#f '(begin ;; -*- geiser-scheme-implementation: mit -*-\n\"HELLO WORLD\"\n))
-
-;; ERROR: <1>: continuation failed "(geiser:eval '#f '(begin ;; -*- geiser-scheme-implementation: mit -*-
-		   
-
-
-;; (use-package flycheck-guile)
+(load "~/.emacs.d/ob-mitscheme")
 
 
 ;; Customize org-mode
@@ -279,7 +210,8 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((R . t)
-   (scheme . t)))
+   (scheme . t)
+   (mitscheme . t)))
 
 (defun ess-org-edit-special-set-R-process ()
   "Automatically sets the R process when entering an org-mode
@@ -293,6 +225,8 @@ process."
 (add-hook 'ess-mode-hook 'ess-org-edit-special-set-R-process)
 
 
+
+
 ;; Global Keyboard Definitions
 (global-set-key (kbd "C-.") 'other-window)
 
@@ -304,8 +238,7 @@ process."
  '(ess-view-data-current-backend 'data\.table+magrittr)
  '(ess-view-data-current-save-backend 'data\.table::fwrite)
  '(package-selected-packages
-   '(lsp-mode geiser-guile ess-view-data hl-todo company ess pdf-tools undo-tree dashboard which-key counsel use-package nano-modeline magit gruvbox-theme doom-modeline))
- '(undo-tree-history-directory-alist '(("\".\"" . "\"~/.emacs.d/undo/"))))
+   '(paredit lsp-mode ess-view-data hl-todo company ess pdf-tools undo-tree dashboard which-key counsel use-package nano-modeline magit gruvbox-theme doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
