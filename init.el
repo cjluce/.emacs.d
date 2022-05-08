b1cfb8ee8f2aefbdb5bb53fb5ddc1775c7ce20d6
cjluce <16cluce@gmail.com>
2022-05-08 00:03:24 -0400

    adding all

--- init.el
+++ init.el
@@ -3,8 +3,6 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(package-initialize)
-
 (unless package-archive-contents
   (package-refresh-contents))
 
@@ -22,6 +20,12 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq inhibit-startup-screen t)
+
+(setq x-select-enable-clipboard t)
+
+(setenv "PATH" (concat (getenv "PATH") ":/home/lucec/.local/bin"))
+(setq exec-path (append exec-path '("/home/lucec/.local/bin")))
+
 
 ;; Misc. Hooks
 (add-hook 'prog-mode-hook 'electric-pair-local-mode)
@@ -97,6 +101,9 @@
     (setq dashboard-items '((recents  . 5)))
     (setq dashboard-banner-logo-title ""))
 
+(if (< (length command-line-args) 2)
+  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))
+
 (use-package undo-tree
   :diminish
   :config
@@ -168,17 +175,55 @@
   :config
   (setq ess-view-data-current-backend 'data\.table+magrittr)
   (setq ess-view-data-current-save-backend 'data\.table::fwrite))
+(require 'ess-view-data)
 
 
 (use-package lsp-mode
   ;; :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   ;; (setq lsp-keymap-prefix "C-c l")
+  :config
+  (setq lsp-idle-delay 0.5
+        lsp-enable-symbol-highlighting t
+        lsp-enable-snippet nil  ;; Not supported by company capf, which is the recommended company backend
+        lsp-pyls-plugins-flake8-enabled t)
+  (lsp-register-custom-settings
+   '(("pyls.plugins.pyls_mypy.enabled" t t)
+     ("pyls.plugins.pyls_mypy.live_mode" nil t)
+     ("pyls.plugins.pyls_black.enabled" t t)
+     ("pyls.plugins.pyls_isort.enabled" t t)
+
+     ;; Disable these as they're duplicated by flake8
+     ("pyls.plugins.pycodestyle.enabled" nil t)
+     ("pyls.plugins.mccabe.enabled" nil t)
+     ("pyls.plugins.pyflakes.enabled" nil t)))
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (R-mode . lsp)
+	 (ess-r-mode . lsp)
          ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
+         (lsp-mode . lsp-enable-which-key-integration)
+	 (js-mode . lsp)
+	 (python-mode . lsp))
   :commands lsp)
+
+(use-package lsp-ui
+  :config
+  (setq lsp-ui-sideline-show-hover t
+                lsp-ui-sideline-delay 0.5
+                lsp-ui-doc-delay 5
+                lsp-ui-sideline-ignore-duplicates t
+                lsp-ui-doc-position 'bottom
+                lsp-ui-doc-alignment 'frame
+                lsp-ui-doc-header nil
+                lsp-ui-doc-include-signature t
+                lsp-ui-doc-use-childframe t)
+  :commands lsp-ui-mode)
+  ;; Probably want to map these...
+  ;; :bind (:map evil-normal-state-map
+  ;;             ("gd" . lsp-ui-peek-find-definitions)
+  ;;             ("gr" . lsp-ui-peek-find-references)
+  ;;             :map md/leader-map
+  ;;             ("Ni" . lsp-ui-imenu))
 
 (use-package paredit
   :hook ((emacs-lisp-mode . paredit-mode)
@@ -187,6 +232,24 @@
 
 (use-package rainbow-delimiters
   :hook ((prog-mode . rainbow-delimiters-mode)))
+
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  ;; (define-key js-mode-map (kbd "C-M-/") 'company-complete)
+  (define-key company-mode-map (kbd "C-M-/") 'company-complete)
+  (setq company-selection-wrap-around t))
+
+;; (use-package company-lsp
+;;   :config
+;;   (push 'company-lsp company-backends))
+
+
+
+
+(use-package json-mode)
+
+(use-package fish-mode)
 
 ;; (use-package telega
 ;;   :ensure nil
@@ -197,6 +260,15 @@
 ;; Add support for mitscheme
 
 (load "~/.emacs.d/ob-mitscheme")
+
+
+(use-package web-mode
+  :mode ("\\.html$" . web-mode))
+(use-package emmet-mode
+  :hook ((sgml-mode-hook . emmet-mode)
+	 (css-mode-hook . emmet-mode)
+	 (web-mode-hook . emmet-mode)))
+
 
 
 ;; Customize org-mode
@@ -238,7 +310,7 @@ process."
  '(ess-view-data-current-backend 'data\.table+magrittr)
  '(ess-view-data-current-save-backend 'data\.table::fwrite)
  '(package-selected-packages
-   '(paredit lsp-mode ess-view-data hl-todo company ess pdf-tools undo-tree dashboard which-key counsel use-package nano-modeline magit gruvbox-theme doom-modeline)))
+   '(lsp-ui fish-mode emmet-mode web-mode json-mode rainbow-delimiters paredit lsp-mode ess-view-data hl-todo company ess pdf-tools undo-tree dashboard which-key counsel use-package nano-modeline magit gruvbox-theme doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
